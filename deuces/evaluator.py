7f330bd13aafa0b12c2a91e240d0c48ac2b6dcae
wangqi <wangqi@WQMacBookPro13.local>
2019-04-21 17:23:52 +0800

    Finish it

--- deuces/evaluator.py
+++ deuces/evaluator.py
@@ -3,226 +3,228 @@ from .card import Card
 from .deck import Deck
 from .lookup import LookupTable
 
+
 class Evaluator(object):
-    """
-    Evaluates hand strengths using a variant of Cactus Kev's algorithm:
-    http://suffe.cool/poker/evaluator.html
+	"""
+	Evaluates hand strengths using a variant of Cactus Kev's algorithm:
+	http://suffe.cool/poker/evaluator.html
 
-    I make considerable optimizations in terms of speed and memory usage, 
-    in fact the lookup table generation can be done in under a second and 
-    consequent evaluations are very fast. Won't beat C, but very fast as 
-    all calculations are done with bit arithmetic and table lookups. 
-    """
+	I make considerable optimizations in terms of speed and memory usage,
+	in fact the lookup table generation can be done in under a second and
+	consequent evaluations are very fast. Won't beat C, but very fast as
+	all calculations are done with bit arithmetic and table lookups.
+	"""
 
-    def __init__(self):
+	def __init__(self):
 
-        self.table = LookupTable()
-        
-        self.hand_size_map = {
-            5 : self._five,
-            6 : self._six,
-            7 : self._seven
-        }
+		self.table = LookupTable()
 
-    def evaluate(self, cards, board):
-        """
-        This is the function that the user calls to get a hand rank. 
+		self.hand_size_map = {
+			5: self._five,
+			6: self._six,
+			7: self._seven
+		}
 
-        Supports empty board, etc very flexible. No input validation 
-        because that's cycles!
-        """
-        all_cards = cards + board
-        return self.hand_size_map[len(all_cards)](all_cards)
+	def evaluate(self, cards, board):
+		"""
+		This is the function that the user calls to get a hand rank.
 
-    def _five(self, cards):
-        """
-        Performs an evalution given cards in integer form, mapping them to
-        a rank in the range [1, 7462], with lower ranks being more powerful.
+		Supports empty board, etc very flexible. No input validation
+		because that's cycles!
+		"""
+		all_cards = cards + board
+		return self.hand_size_map[len(all_cards)](all_cards)
 
-        Variant of Cactus Kev's 5 card evaluator, though I saved a lot of memory
-        space using a hash table and condensing some of the calculations. 
-        """
-        # if flush
-        if cards[0] & cards[1] & cards[2] & cards[3] & cards[4] & 0xF000:
-            handOR = (cards[0] | cards[1] | cards[2] | cards[3] | cards[4]) >> 16
-            prime = Card.prime_product_from_rankbits(handOR)
-            return self.table.flush_lookup[prime]
+	def _five(self, cards):
+		"""
+		Performs an evalution given cards in integer form, mapping them to
+		a rank in the range [1, 7462], with lower ranks being more powerful.
 
-        # otherwise
-        else:
-            prime = Card.prime_product_from_hand(cards)
-            return self.table.unsuited_lookup[prime]
+		Variant of Cactus Kev's 5 card evaluator, though I saved a lot of memory
+		space using a hash table and condensing some of the calculations.
+		"""
+		# if flush
+		if cards[0] & cards[1] & cards[2] & cards[3] & cards[4] & 0xF000:
+			handOR = (cards[0] | cards[1] | cards[2] | cards[3] | cards[4]) >> 16
+			prime = Card.prime_product_from_rankbits(handOR)
+			return self.table.flush_lookup[prime]
 
-    def _six(self, cards):
-        """
-        Performs five_card_eval() on all (6 choose 5) = 6 subsets
-        of 5 cards in the set of 6 to determine the best ranking, 
-        and returns this ranking.
-        """
-        minimum = LookupTable.MAX_HIGH_CARD
+		# otherwise
+		else:
+			prime = Card.prime_product_from_hand(cards)
+			return self.table.unsuited_lookup[prime]
 
-        all5cardcombobs = itertools.combinations(cards, 5)
-        for combo in all5cardcombobs:
+	def _six(self, cards):
+		"""
+		Performs five_card_eval() on all (6 choose 5) = 6 subsets
+		of 5 cards in the set of 6 to determine the best ranking,
+		and returns this ranking.
+		"""
+		minimum = LookupTable.MAX_HIGH_CARD
 
-            score = self._five(combo)
-            if score < minimum:
-                minimum = score
+		all5cardcombobs = itertools.combinations(cards, 5)
+		for combo in all5cardcombobs:
 
-        return minimum
+			score = self._five(combo)
+			if score < minimum:
+				minimum = score
 
-    def _seven(self, cards):
-        """
-        Performs five_card_eval() on all (7 choose 5) = 21 subsets
-        of 5 cards in the set of 7 to determine the best ranking, 
-        and returns this ranking.
-        """
-        minimum = LookupTable.MAX_HIGH_CARD
+		return minimum
 
-        all5cardcombobs = itertools.combinations(cards, 5)
-        for combo in all5cardcombobs:
-            
-            score = self._five(combo)
-            if score < minimum:
-                minimum = score
+	def _seven(self, cards):
+		"""
+		Performs five_card_eval() on all (7 choose 5) = 21 subsets
+		of 5 cards in the set of 7 to determine the best ranking,
+		and returns this ranking.
+		"""
+		minimum = LookupTable.MAX_HIGH_CARD
 
-        return minimum
+		all5cardcombobs = itertools.combinations(cards, 5)
+		for combo in all5cardcombobs:
 
-    def get_rank_class(self, hr):
-        """
-        Returns the class of hand given the hand hand_rank
-        returned from evaluate. 
-        """
-        if hr >= 0 and hr <= LookupTable.MAX_STRAIGHT_FLUSH:
-            return LookupTable.MAX_TO_RANK_CLASS[LookupTable.MAX_STRAIGHT_FLUSH]
-        elif hr <= LookupTable.MAX_FOUR_OF_A_KIND:
-            return LookupTable.MAX_TO_RANK_CLASS[LookupTable.MAX_FOUR_OF_A_KIND]
-        elif hr <= LookupTable.MAX_FULL_HOUSE:
-            return LookupTable.MAX_TO_RANK_CLASS[LookupTable.MAX_FULL_HOUSE]
-        elif hr <= LookupTable.MAX_FLUSH:
-            return LookupTable.MAX_TO_RANK_CLASS[LookupTable.MAX_FLUSH]
-        elif hr <= LookupTable.MAX_STRAIGHT:
-            return LookupTable.MAX_TO_RANK_CLASS[LookupTable.MAX_STRAIGHT]
-        elif hr <= LookupTable.MAX_THREE_OF_A_KIND:
-            return LookupTable.MAX_TO_RANK_CLASS[LookupTable.MAX_THREE_OF_A_KIND]
-        elif hr <= LookupTable.MAX_TWO_PAIR:
-            return LookupTable.MAX_TO_RANK_CLASS[LookupTable.MAX_TWO_PAIR]
-        elif hr <= LookupTable.MAX_PAIR:
-            return LookupTable.MAX_TO_RANK_CLASS[LookupTable.MAX_PAIR]
-        elif hr <= LookupTable.MAX_HIGH_CARD:
-            return LookupTable.MAX_TO_RANK_CLASS[LookupTable.MAX_HIGH_CARD]
-        else:
-            raise Exception("Inavlid hand rank, cannot return rank class")
+			score = self._five(combo)
+			if score < minimum:
+				minimum = score
 
-    def class_to_string(self, class_int):
-        """
-        Converts the integer class hand score into a human-readable string.
-        """
-        return LookupTable.RANK_CLASS_TO_STRING[class_int]
+		return minimum
 
-    def get_five_card_rank_percentage(self, hand_rank):
-        """
-        Scales the hand rank score to the [0.0, 1.0] range.
-        """
-        return float(hand_rank) / float(LookupTable.MAX_HIGH_CARD)
+	def get_rank_class(self, hr):
+		"""
+		Returns the class of hand given the hand hand_rank
+		returned from evaluate.
+		"""
+		if hr >= 0 and hr <= LookupTable.MAX_STRAIGHT_FLUSH:
+			return LookupTable.MAX_TO_RANK_CLASS[LookupTable.MAX_STRAIGHT_FLUSH]
+		elif hr <= LookupTable.MAX_FOUR_OF_A_KIND:
+			return LookupTable.MAX_TO_RANK_CLASS[LookupTable.MAX_FOUR_OF_A_KIND]
+		elif hr <= LookupTable.MAX_FULL_HOUSE:
+			return LookupTable.MAX_TO_RANK_CLASS[LookupTable.MAX_FULL_HOUSE]
+		elif hr <= LookupTable.MAX_FLUSH:
+			return LookupTable.MAX_TO_RANK_CLASS[LookupTable.MAX_FLUSH]
+		elif hr <= LookupTable.MAX_STRAIGHT:
+			return LookupTable.MAX_TO_RANK_CLASS[LookupTable.MAX_STRAIGHT]
+		elif hr <= LookupTable.MAX_THREE_OF_A_KIND:
+			return LookupTable.MAX_TO_RANK_CLASS[LookupTable.MAX_THREE_OF_A_KIND]
+		elif hr <= LookupTable.MAX_TWO_PAIR:
+			return LookupTable.MAX_TO_RANK_CLASS[LookupTable.MAX_TWO_PAIR]
+		elif hr <= LookupTable.MAX_PAIR:
+			return LookupTable.MAX_TO_RANK_CLASS[LookupTable.MAX_PAIR]
+		elif hr <= LookupTable.MAX_HIGH_CARD:
+			return LookupTable.MAX_TO_RANK_CLASS[LookupTable.MAX_HIGH_CARD]
+		else:
+			raise Exception("Inavlid hand rank, cannot return rank class")
 
-    def hand_summary(self, board, hands):
-        """
-        Gives a sumamry of the hand with ranks as time proceeds. 
+	def class_to_string(self, class_int):
+		"""
+		Converts the integer class hand score into a human-readable string.
+		"""
+		return LookupTable.RANK_CLASS_TO_STRING[class_int]
 
-        Requires that the board is in chronological order for the 
-        analysis to make sense.
-        """
+	def get_five_card_rank_percentage(self, hand_rank):
+		"""
+		Scales the hand rank score to the [0.0, 1.0] range.
+		"""
+		return float(hand_rank) / float(LookupTable.MAX_HIGH_CARD)
 
-        assert len(board) == 5, "Invalid board length"
-        for hand in hands:
-            assert len(hand) == 2, "Inavlid hand length"
+	def hand_summary(self, board, hands):
+		"""
+		Gives a sumamry of the hand with ranks as time proceeds.
 
-        line_length = 10
-        stages = ["FLOP", "TURN", "RIVER"]
+		Requires that the board is in chronological order for the
+		analysis to make sense.
+		"""
 
-        for i in range(len(stages)):
-            line = ("=" * line_length) + " %s " + ("=" * line_length) 
-            print(line % stages[i])
-            
-            best_rank = 7463  # rank one worse than worst hand
-            winners = []
-            for player, hand in enumerate(hands):
+		assert len(board) == 5, "Invalid board length"
+		for hand in hands:
+			assert len(hand) == 2, "Inavlid hand length"
 
-                # evaluate current board position
-                rank = self.evaluate(hand, board[:(i + 3)])
-                rank_class = self.get_rank_class(rank)
-                class_string = self.class_to_string(rank_class)
-                percentage = 1.0 - self.get_five_card_rank_percentage(rank)  # higher better here
-                print("Player %d hand = %s, percentage rank among all hands = %f" % (
-                    player + 1, class_string, percentage))
+		line_length = 10
+		stages = ["FLOP", "TURN", "RIVER"]
 
-                # detect winner
-                if rank == best_rank:
-                    winners.append(player)
-                    best_rank = rank
-                elif rank < best_rank:
-                    winners = [player]
-                    best_rank = rank
+		for i in range(len(stages)):
+			line = ("=" * line_length) + " %s " + ("=" * line_length)
+			print(line % stages[i])
 
-            # if we're not on the river
-            if i != stages.index("RIVER"):
-                if len(winners) == 1:
-                    print("Player %d hand is currently winning.\n" % (winners[0] + 1,))
-                else:
-                    print("Players %s are tied for the lead.\n" % [x + 1 for x in winners])
+			best_rank = 7463  # rank one worse than worst hand
+			winners = []
+			for player, hand in enumerate(hands):
 
-            # otherwise on all other streets
-            else:
-                print(("=" * line_length) + " HAND OVER " + ("=" * line_length))
-                if len(winners) == 1:
-                    print("Player %d is the winner with a %s\n" % (winners[0] + 1,
-                        self.class_to_string(self.get_rank_class(self.evaluate(hands[winners[0]], board)))))
-                else:
-                    print("Players %s tied for the win with a %s\n" % (winners,
-                        self.class_to_string(self.get_rank_class(self.evaluate(hands[winners[0]], board)))))
+				# evaluate current board position
+				rank = self.evaluate(hand, board[:(i + 3)])
+				rank_class = self.get_rank_class(rank)
+				class_string = self.class_to_string(rank_class)
+				percentage = 1.0 - self.get_five_card_rank_percentage(rank)  # higher better here
+				print("Player %d hand = %s, percentage rank among all hands = %f" % (
+					player + 1, class_string, percentage))
 
-    def hand_evaluate(self, board, players, debug=True):
-        """
-        Gives a sumamry of the hand with ranks as time proceeds.
+				# detect winner
+				if rank == best_rank:
+					winners.append(player)
+					best_rank = rank
+				elif rank < best_rank:
+					winners = [player]
+					best_rank = rank
 
-        Requires that the board is in chronological order for the
-        analysis to make sense.
-        """
+			# if we're not on the river
+			if i != stages.index("RIVER"):
+				if len(winners) == 1:
+					print("Player %d hand is currently winning.\n" % (winners[0] + 1,))
+				else:
+					print("Players %s are tied for the lead.\n" % [x + 1 for x in winners])
 
-        assert len(board) == 5, "Invalid board length"
-        for player in players:
-            hand = player.cards
-            assert len(hand) == 2, "Inavlid hand length"
+			# otherwise on all other streets
+			else:
+				print(("=" * line_length) + " HAND OVER " + ("=" * line_length))
+				if len(winners) == 1:
+					print("Player %d is the winner with a %s\n" % (winners[0] + 1,
+					                                               self.class_to_string(self.get_rank_class(
+						                                               self.evaluate(hands[winners[0]], board)))))
+				else:
+					print("Players %s tied for the win with a %s\n" % (winners,
+					                                                   self.class_to_string(self.get_rank_class(
+						                                                   self.evaluate(hands[winners[0]], board)))))
 
-        stages = ["FLOP", "TURN", "RIVER"]
+	def hand_evaluate(self, board, players, debug=True):
+		"""
+		Gives a sumamry of the hand with ranks as time proceeds.
 
-        winner_player = None
-        for i in range(len(stages)):
-            best_rank = 7463  # rank one worse than worst hand
-            winners = {}
-            for idx, player in enumerate(players):
-                hand = player.cards
-                # evaluate current board position
-                rank = self.evaluate(hand, board[:(i + 3)])
-                rank_class = self.get_rank_class(rank)
-                class_string = self.class_to_string(rank_class)
-                percentage = 1.0 - self.get_five_card_rank_percentage(rank)  # higher better here
+		Requires that the board is in chronological order for the
+		analysis to make sense.
+		"""
 
-                # detect winner
-                if rank == best_rank:
-                    winners['tie'] = player
-                    best_rank = rank
-                    winner_player = player
-                elif rank < best_rank:
-                    winners = {'win': player}
-                    best_rank = rank
-                    winner_player = player
+		assert len(board) == 5, "Invalid board length"
+		for player in players:
+			hand = player.cards
+			assert len(hand) == 2, "Inavlid hand length"
 
-        winner_method = self.class_to_string(self.get_rank_class(self.evaluate(winner_player.cards, board)))
+		stages = ["FLOP", "TURN", "RIVER"]
 
-        if debug:
-            print("Board cards: " + Card.format_pretty_cards(board))
-            print("Winner cards:" + Card.format_pretty_cards(winner_player.cards))
-            print("Winner method: ", winner_method)
-        return (winner_method, winner_player)
+		winner_player = None
+		for i in range(len(stages)):
+			best_rank = 7463  # rank one worse than worst hand
+			winners = {}
+			for idx, player in enumerate(players):
+				hand = player.cards
+				# evaluate current board position
+				rank = self.evaluate(hand, board[:(i + 3)])
+				rank_class = self.get_rank_class(rank)
+				class_string = self.class_to_string(rank_class)
+				percentage = 1.0 - self.get_five_card_rank_percentage(rank)  # higher better here
 
+				# detect winner
+				if rank == best_rank:
+					winners['tie'] = player
+					best_rank = rank
+					winner_player = player
+				elif rank < best_rank:
+					winners = {'win': player}
+					best_rank = rank
+					winner_player = player
+
+		winner_method = self.class_to_string(self.get_rank_class(self.evaluate(winner_player.cards, board)))
+
+		if debug:
+			print("Board cards: " + Card.format_pretty_cards(board))
+			print("Winner cards:" + Card.format_pretty_cards(winner_player.cards))
+			print("Winner method: ", winner_method)
+		return (winner_method, winner_player)
--- play_solo.py
+++ play_solo.py
@@ -1,6 +1,9 @@
 from deuces import Card, Evaluator, Deck, Round, Player
 import os
 
+
+round = Round.read_status()
+
 PRIORITIES = [
 	"Straight Flush",
     "Four of a Kind",
@@ -12,8 +15,6 @@ PRIORITIES = [
     "Pair",
     "High Card"
 ]
-
-round = Round.read_status()
 
 if round is not None and round.num_player > 0:
 	restore_last_round = input("Do you want to restore last round (y/n) ? \n")
@@ -37,45 +38,14 @@ print("> total %d players" % num_players)
 if round is None:
 	round = Round(num_players)
 
-players = []
 deck = Deck()
-for player_id in range(num_players):
-	player = Player(player_id, deck=deck)
-	default_init_cards = round.get_player_cards(player_id, player.name)
-	init_card_str = input("Input the initial 2 cards for player.py: %s. Last: %s \n" %
-	                      (player.name, default_init_cards))
-	if len(init_card_str) == 0:
-		if len(default_init_cards) == 0:
-			card_ints = player.draw_cards(num=2)
-			round.add_player_card_ints(player_id, player.name, card_ints)
-		else:
-			cards = default_init_cards.split(",")
-			player.draw_cards(cards)
-			round.add_player_cards(player_id=player_id, player_name=player.name, card_strs=default_init_cards)
-	else:
-		init_cards = init_card_str.split(",")
-		player.draw_cards(init_cards)
-		round.add_player_cards(player_id=player_id, player_name=player.name, card_strs=init_cards)
-	players.append(player)
-
-for player in players:
-	print(player)
-
-flop_card_input = input("Input the flop cards. Last: " + round.get_flop_cards())
-flop_card_ints = []
-if flop_card_input is None or len(flop_card_input) == 0:
-	flop_card_input = round.get_flop_cards()
-if len(flop_card_input) == 0:
-	flop_card_ints = deck.flop()
-	for flop_card_int in flop_card_ints:
-		round.add_flop_card(flop_card_int)
-else:
-	flop_card_strs = flop_card_input.split(",")
-	flop_card_ints = deck.flop_card(flop_card_strs)
-print("Flops: " + Card.format_pretty_cards(flop_card_ints))
-print("Total left cards in deck: %d" % deck.left_card_num())
-
-round.save_status()
+first_player = Player(0, deck=deck)
+default_init_cards = round.get_player_cards(0, first_player.name)
+init_card_str = input("Input the initial 2 cards for player: %s. Last: %s \n" %
+                      (first_player.name, default_init_cards))
+cards = default_init_cards.split(",")
+first_player.draw_cards(cards)
+print(first_player)
 
 print("\n=== Begin Simulation ===")
 result = {}
@@ -84,8 +54,19 @@ evaluator = Evaluator()
 winner_methods = {}
 winner_players = {}
 deck.save()
+
 for i in range(0, 10000):
 	deck.reset()
+	players = []
+	players.append(first_player)
+	for player_id in range(1, num_players, 1):
+		player = Player(player_id, deck=deck)
+		card_ints = player.draw_cards(num=2)
+		players.append(player)
+	flop_card_ints = deck.flop()
+	# print("Flops: " + Card.format_pretty_cards(flop_card_ints))
+	# print("Total left cards in deck: %d" % deck.left_card_num())
+
 	flop_card_ints = deck.get_flop_card_ints()
 	turn_card_int = deck.draw()
 	flop_card_ints.append(turn_card_int)
@@ -114,4 +95,4 @@ print("{:<20} {:<10} {:<10}".format('Player','Count','Percent'))
 for i in range(num_players):
 	k = "player-" + str(i)
 	v = winner_players.get(k, 0)
-	print("{:<20} {:<10} {:<10}".format(k, v, str(v/100.0)+"%"))
+	print("{:<20} {:<10} {:<10}".format(k, v, str(v / 100.0) + "%"))
